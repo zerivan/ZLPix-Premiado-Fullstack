6702e41576089bb751532ee9abfacd6ded1efccd
Zerivan Lima <zerivanlima@gmail.com>
2025-11-09 17:46:18 -0300

    fix: corrigir tsconfig.build.json válido e funcional

---
+++ backend/dist/Index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import pixroutes from "./routes/pixroutes";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/pix", pixroutes);
+app.get("/", (_, res) => {
+    res.send("🚀 Servidor ZLPix Premiado rodando com sucesso!");
+});
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+    console.log(`✅ Servidor backend rodando na porta ${PORT}`);
+});
