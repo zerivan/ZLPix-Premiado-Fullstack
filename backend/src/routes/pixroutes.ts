cd8b1d76f499322ccb2255acb065913ca3b12da2
zerivan <zeri41@hotmail.com>
2025-11-09 10:07:53 -0300

    Atualizar o pixroutes.ts

--- backend/src/routes/pixroutes.ts
+++ backend/src/routes/pixroutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { gerarPix } from "../controllers/pixController";
+import { gerarPix } from "../controllers/pixcontrollers";
 
 const router = Router();
 
+// Rota para gerar um código Pix
 router.post("/gerar", gerarPix);
 
 export default router;
