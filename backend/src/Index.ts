824dee1bd908776dcbab0bd7b03005ad3e2bc162
zerivan <zeri41@hotmail.com>
2025-11-09 10:06:20 -0300

    Atualizar o Index.ts

--- backend/src/Index.ts
+++ backend/src/Index.ts
@@ -1,19 +1,18 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import pixRoutes from "./routes/pixRoutes";
+import pixroutes from "./routes/pixroutes";
 
-dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/pix", pixRoutes);
+app.use("/pix", pixroutes);
 
 app.get("/", (_, res) => {
-  res.send("🚀 Backend do ZLPix Premiado está rodando!");
+  res.send("🚀 Servidor ZLPix Premiado rodando com sucesso!");
 });
 
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`✅ Servidor rodando na porta ${PORT}`));
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+  console.log(`✅ Servidor backend rodando na porta ${PORT}`);
+});
