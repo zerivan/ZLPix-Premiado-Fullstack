5334e96bd05d352ad38ec01c8296f860855c85fc
zerivan <zeri41@hotmail.com>
2025-11-15 14:57:04 -0300

    Atualizar o vite.config.ts

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "",              // 👈 ESSA LINHA RESOLVE O PROBLEMA DE CAMINHOS!
   plugins: [react()],
   build: { outDir: "dist" },
   server: { port: 5173 }
