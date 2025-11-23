1aed491a5f7683a33bea82ee54bf0b3f26857aba
zerivan <zeri41@hotmail.com>
2025-11-23 11:28:38 -0300

    Replace App component with Routes component

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app";
 import "./styles/index.css";
 
 import { api } from "./api/client";
+import Routes from "./routes/index";
 
 api.get("/")
   .then(() => console.log("✅ Conectado ao backend com sucesso!"))
-  .catch((err) => console.error("❌ Erro ao conectar ao backend:", err.message));
+  .catch((err) =>
+    console.error("❌ Erro ao conectar ao backend:", err.message)
+  );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>
 );
