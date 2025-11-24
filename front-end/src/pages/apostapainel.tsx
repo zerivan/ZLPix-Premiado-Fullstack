4eccac6b657bc8d45a62156240840744912a6dd4
zerivan <zeri41@hotmail.com>
2025-11-23 21:56:08 -0300

    Atualizar o apostapainel.tsx

--- front-end/src/pages/apostapainel.tsx
+++ front-end/src/pages/apostapainel.tsx
@@ -1,7 +1,7 @@
 // src/apostapainel.tsx
 import React, { useEffect, useRef, useState } from "react";
-import Header from "./components/Header"; // ajuste se o seu header estiver em outro path
-import NavBottom from "./components/NavBottom"; // ajuste se o seu nav estiver em outro path
+import header from "./components/header"; // ajuste se o seu header estiver em outro path
+import navbottom from "./components/navbottom"; // ajuste se o seu nav estiver em outro path
 import { useNavigate } from "react-router-dom";
 
 /**
