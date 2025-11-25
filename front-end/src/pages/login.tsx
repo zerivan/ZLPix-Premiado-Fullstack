7d8994723e4c8cd41df06dffbc70f810945cfd9b
zerivan <zeri41@hotmail.com>
2025-11-24 23:17:16 -0300

    Atualizar o login.tsx

--- front-end/src/pages/login.tsx
+++ front-end/src/pages/login.tsx
@@ -7,52 +7,55 @@ export default function Login() {
   const [senha, setSenha] = useState("");
 
   return (
-    <div className="login-wrapper">
-      <div className="login-box">
+    <div className="page-wrapper">
 
+      <div className="page-card">
+        
         {/* LOGO */}
-        <div className="logo-area">
+        <div className="login-logo-box">
           <img
             src="https://lh3.googleusercontent.com/aida-public/AB6AXuBmzrE-Lxoj0vhBEQ06zXmsjgkqYG5YBlM1M9_v6HQ4R4pBfd3yVEEpnp5XPqZRHsJ6dWz1JuQc02890lsQdUljWDlvoMImtzkLgrs2rfv3QL-NrsYiDAzqkXhSdT8rRM9Qu4lphwOalWJNxxBix-212vwFBaU03M53Jrbx14xLnkofjbeXCG_e18RNUcOeh3Cl6sQoV0aDgBHDCX3qM0OG6PFoATVuZ5ban3RA7_evH4W8Qm3m3rKyvSn-shgPw2K9K306pNEzHak"
             alt="Logo ZLPix"
-            className="logo-img"
+            className="login-logo"
           />
         </div>
 
         {/* TÍTULO */}
-        <h1 className="login-title">Entrar na sua conta</h1>
-        <p className="login-subtitle">Aposte e acompanhe seus resultados</p>
+        <h1 className="page-title">Entrar na sua conta</h1>
+        <p className="page-subtitle">Aposte e acompanhe seus resultados</p>
 
         {/* CAMPOS */}
-        <label className="label">E-mail</label>
+        <label className="page-label">E-mail</label>
         <input
           type="email"
           placeholder="seuemail@exemplo.com"
+          className="page-input"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
 
-        <label className="label">Senha</label>
+        <label className="page-label">Senha</label>
         <input
           type="password"
           placeholder="Digite sua senha"
+          className="page-input"
           value={senha}
           onChange={(e) => setSenha(e.target.value)}
         />
 
         {/* BOTÃO LOGIN */}
         <button
-          className="btn btn-primary full-btn"
+          className="page-btn"
           onClick={() => navigate("/")}
         >
           Entrar
         </button>
 
         {/* LINK PARA CADASTRO */}
-        <p className="register-text">
+        <p className="page-register">
           Não tem conta?
           <span
-            className="register-link"
+            className="page-link"
             onClick={() => navigate("/cadastro")}
           >
             Cadastre-se
