5c516cf8d60a43c4508086c30f7fda378953d067
zerivan <zeri41@hotmail.com>
2025-11-26 10:02:43 -0300

    Atualizar o prisma.ts

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -2,12 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 declare global {
   // Evita múltiplas instâncias no modo dev
+  // (precisa usar "var" no global em TS)
   var prisma: PrismaClient | undefined;
 }
 
 export const prisma =
   global.prisma ||
   new PrismaClient({
+    // Prisma 7 agora exige datasource aqui
+    datasourceUrl: process.env.DATABASE_URL,
     log: ["query", "error", "warn"],
   });
 
