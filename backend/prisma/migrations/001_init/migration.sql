9713785f9b2e2d80d994a49118c686535e0a3458
zerivan <zeri41@hotmail.com>
2025-11-28 17:52:15 -0300

    Delete backend/prisma/migrations/001_init directory

--- backend/prisma/migrations/001_init/migration.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "User" (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    phone TEXT,
-    pixKey TEXT,
-    passwordHash TEXT NOT NULL,
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
