79b2ab31dbf9f7cddcaca38d920afefcd8de3b63
zerivan <zeri41@hotmail.com>
2025-11-29 09:13:40 -0300

    Atualizar o prisma.ts

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,20 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
-declare global {
-  // eslint-disable-next-line no-var
-  var __prisma: PrismaClient | undefined;
-}
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.__prisma) {
-    global.__prisma = new PrismaClient();
-  }
-  prisma = global.__prisma;
-}
+const prisma = new PrismaClient();
 
 export default prisma;
 export { prisma };
