1a427c307345f7eb9e90a62c6f47792e46c52ac2
* <zerivan@example.com>
2025-12-01 09:02:20 -0300

    fix: remover configs antigos do Prisma 7 e manter compatibilidade Prisma 6

--- backend/prisma.confg.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "@prisma/config";
-
-export default defineConfig({
-  datasource: {
-    // Aqui fica a URL do banco agora
-    url: process.env.DATABASE_URL!,
-  },
-});
