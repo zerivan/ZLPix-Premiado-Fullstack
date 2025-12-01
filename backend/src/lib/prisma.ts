519ab161b565111df38ec578cf7cc600786ea012
zerivan <zeri41@hotmail.com>
2025-12-01 10:09:49 -0300

    Atualizar o prisma.ts

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,6 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"], // evita excesso de logs no Render
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
-export { prisma };
