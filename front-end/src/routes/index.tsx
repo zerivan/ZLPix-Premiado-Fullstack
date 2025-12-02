0243d43152f7f692b701b926491e451397d4e8e1
zerivan <zeri41@hotmail.com>
2025-12-02 19:03:50 -0300

    Atualizar o index.tsx

--- front-end/src/routes/index.tsx
+++ front-end/src/routes/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 // 🧭 Páginas
-import Home from "../pages/home";
+import Home from "../pages/Home"; // ✅ Corrigido (H maiúsculo — compatível com Render)
 import Login from "../pages/login";
 import Cadastro from "../pages/cadastro";
 import ApostaPainel from "../pages/apostapainel";
