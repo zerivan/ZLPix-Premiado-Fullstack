8a1cd727d5b1e3810845a25ddb38a53f342c9bf1
zerivan <zeri41@hotmail.com>
2025-12-16 20:41:30 -0300

    Atualizar o index.tsx

--- front-end/src/routes/index.tsx
+++ front-end/src/routes/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import approutes from "./routes/approutes";
+import AppRoutes from "./routes/approutes"; // ✅ apenas mudou o nome
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <approutes />
+      <AppRoutes /> {/* ✅ componente React válido */}
     </BrowserRouter>
   </React.StrictMode>
 );
