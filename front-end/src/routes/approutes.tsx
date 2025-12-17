1f4216838559dd9372fcf3d6eec2c33f635c2586
zerivan <zeri41@hotmail.com>
2025-12-17 00:23:48 -0300

    Atualizar o approutes.tsx

--- front-end/src/routes/approutes.tsx
+++ front-end/src/routes/approutes.tsx
@@ -15,7 +15,7 @@ import RecuperarSenha from "../pages/recuperar-senha";
 
 // Admin
 import AdminRoute from "../components/adminroute";
-import AdminDashboard from "../pages/admindashboard";
+import AdminDashboard from "../dashboard"; // ✅ CAMINHO REAL
 
 // Auxiliares
 import Revisao from "../pages/revisao";
