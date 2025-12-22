132d71c0a3aff33f81b45541931031e1b3970cd6
zerivan <zeri41@hotmail.com>
2025-12-22 17:53:29 -0300

    Atualizar o client.ts

--- front-end/src/api/client.ts
+++ front-end/src/api/client.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.VITE_API_URL ||
+  "https://zlpix-premiado-fullstack.onrender.com";
+
 export const api = axios.create({
-  baseURL: "https://zlpix-premiado-fullstack.onrender.com",
+  baseURL,
+});
+
+// segurança extra (não interfere no admin)
+api.interceptors.request.use((config) => {
+  config.headers = config.headers || {};
+  return config;
 });
