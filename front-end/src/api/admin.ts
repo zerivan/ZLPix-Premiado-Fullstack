80ddf835be4d18cbf2c73da8691ebd913346b90d
zerivan <zeri41@hotmail.com>
2025-12-22 17:48:22 -0300

    Atualizar o admin.ts

--- front-end/src/api/admin.ts
+++ front-end/src/api/admin.ts
@@ -6,10 +6,14 @@ export const adminApi = axios.create({
 
 // sempre envia o token do admin
 adminApi.interceptors.request.use((config) => {
-  const token = localStorage.getItem("TOKEN_ZLPIX_ADMIN");
+  // proteção para ambiente onde localStorage não existe
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("TOKEN_ZLPIX_ADMIN");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
 
   return config;
