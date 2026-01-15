fd1724fb07f279c364b2985d64a1875144f93b04
zerivan <zeri41@hotmail.com>
2026-01-15 00:20:45 -0300

    Atualizar o adminconteudocontrol.tsx

--- front-end/src/components/adminconteudocontrol.tsx
+++ front-end/src/components/adminconteudocontrol.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import EditorQuill from "../../editor/EditorQuill";
+import EditorQuill from "../editor/editorquill";
 
 type CmsArea = {
   key: string;
