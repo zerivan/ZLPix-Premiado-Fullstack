6efebe7bf109e5a4e12673d5e6c94ad992568698
zerivan <zeri41@hotmail.com>
2026-01-24 18:57:33 -0300

    Delete backend/test-bcrypt.js

--- backend/test-bcrypt.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-(async () => {
-  const hash = "$2b$10$wK3lJZJ3WlXK8g0s3qQ5eOeF9m1kYk8N8XbZ2Qv1LwXy9zQeYp2bS";
-  const senha = "Admin@2026#Segura";
-
-  const result = await bcrypt.compare(senha, hash);
-  console.log("Senha confere?", result);
-})();
