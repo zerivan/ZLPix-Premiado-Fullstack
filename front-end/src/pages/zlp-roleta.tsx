9cc93f7bad7e95b3e7329cefe49fe33778e90200
zerivan <zeri41@hotmail.com>
2026-05-31 12:55:00 -0300

    Atualizar o zlp-roleta.tsx

--- front-end/src/pages/zlp-roleta.tsx
+++ front-end/src/pages/zlp-roleta.tsx
@@ -146,7 +146,7 @@ export default function ZLPRoletaPage() {
   
   function calcularSetor(grau: number) {  
     const tamanho = 360 / setores.length;  
-    const ajustado = (360 - ((grau + 270) % 360)) % 360;  
+    const ajustado = (360 - (grau % 360)) % 360;  
     const index = Math.floor(ajustado / tamanho);  
     return setores[index] ?? setores[1];  
   }  
@@ -175,7 +175,7 @@ export default function ZLPRoletaPage() {
   try {
     const res = await api.post(
       "/zlp/roleta",
-      { valor: setor.premio },
+      { premio: setor.premio },
       { headers: { "x-user-id": userId } }
     );
 
