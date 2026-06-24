440849c75555d0f04365889adc42a39f95f17688
zerivan <zeri41@hotmail.com>
2026-06-23 23:12:08 -0300

    Atualizar o capacitor.config.ts

--- front-end/capacitor.config.ts
+++ front-end/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.zlpix.premiado',
   appName: 'ZLPix Premiado',
-  webDir: 'dist'
+  webDir: 'dist',
+
+  server: {
+    url: 'https://zlpixpremiado.com.br'
+  }
 };
 
 export default config;
