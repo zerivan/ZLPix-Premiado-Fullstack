ca8767c71331fc1b8cb80b33e44ee8558bdc880e
zerivan <zeri41@hotmail.com>
2026-06-23 23:00:39 -0300

    Add files via upload

---
+++ front-end/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zlpix.premiado',
+  appName: 'ZLPix Premiado',
+  webDir: 'dist'
+};
+
+export default config;
